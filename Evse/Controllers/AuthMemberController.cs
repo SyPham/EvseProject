8f333488d6fcddac4fa47fa675aa91adfe1c92e0
HenryPham <phamtiensy8696@gmail.com>
2023-03-07 00:37:47 +0700

    add member authen

---
+++ Evse/Controllers/AuthMemberController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Evse.DTO;
+using Evse.DTO.auth;
+using Evse.Services;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Evse.Controllers
+{
+    public class AuthMemberController : ApiControllerBase
+    {
+        private readonly IAuthMemberService _authService;
+
+        public AuthMemberController(
+            IAuthMemberService authService
+            )
+        {
+            _authService = authService;
+        }
+        [HttpPost]
+        public async Task<IActionResult> RefreshTokenAsync([FromBody] RefreshTokenViewModel model)
+        {
+            return StatusCodeResult(await _authService.RefreshTokenAsync(model.token, model.refreshToken));
+        }
+       
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> LoginAsync([FromBody] UserForLoginDto model)
+        {
+            return StatusCodeResult(await _authService.LoginAsync(model));
+        }
+
+       
+        [HttpPost]
+        public async Task<IActionResult> LoginRememberAsync([FromBody] UserForLoginRememberDto request)
+        {
+            return StatusCodeResult(await _authService.LoginAsync(request.ID));
+        }
+       
+
+       
+
+        [HttpGet]
+        public async Task<IActionResult> ForgotPassword(string email)
+        {
+            return Ok(await _authService.ForgotPassword(email));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(ResetPasswordDto reset)
+        {
+            return StatusCodeResult(await _authService.ResetPassword(reset));
+        }
+        [HttpPost]
+        public async Task<IActionResult> ForgotUsername(ForgotUsernameDto forgot)
+        {
+            return StatusCodeResult(await _authService.ForgotUsername(forgot.email));
+        }
+        [HttpPost]
+        public async Task<IActionResult> LogOutAsync()
+        {
+            await _authService.LogOut();
+            return NoContent();
+        }
+       
+    }
+
+}
--- Evse/DTO/auth/UserForDetailDto.cs
+++ Evse/DTO/auth/UserForDetailDto.cs
@@ -22,4 +22,18 @@ namespace Evse.DTO.auth
         public object PageSizeSettingValue { get; set; }
         public object PageSizeSettingList { get; set; }
     }
+    public class UserMemberForDetailDto
+    {
+        public object ID { get; set; }
+        public string Guid { get; set; }
+        public string Username { get; set; }
+        public string AccountName { get; set; }
+        public string NickName { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string FullName { get; set; }
+        public string Area { get; set; }
+        public string Avatar { get; set; }
+     
+    }
 }
