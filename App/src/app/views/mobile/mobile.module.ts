3de68f971ca6bea951bd9cd1f00ea420d1fc0e5f
HenryPham <phamtiensy8696@gmail.com>
2023-06-15 22:33:06 +0700

    fix cldr-data/supplemental/weekdata.json

--- App/src/app/views/mobile/mobile.module.ts
+++ App/src/app/views/mobile/mobile.module.ts
@@ -88,21 +88,21 @@ loadCldr(
   require('cldr-data/main/en/ca-gregorian.json'),
   require('cldr-data/main/en/numbers.json'),
   require('cldr-data/main/en/timeZoneNames.json'),
-  require('cldr-data/supplemental/weekdata.json')); 
+  require('cldr-data/supplemental/weekData.json')); 
 
 loadCldr(
   require('cldr-data/supplemental/numberingSystems.json'),
   require('cldr-data/main/zh/ca-gregorian.json'),
   require('cldr-data/main/zh/numbers.json'),
   require('cldr-data/main/zh/timeZoneNames.json'),
-  require('cldr-data/supplemental/weekdata.json')); 
+  require('cldr-data/supplemental/weekData.json')); 
 
   loadCldr(
     require('cldr-data/supplemental/numberingSystems.json'),
     require('cldr-data/main/vi/ca-gregorian.json'),
     require('cldr-data/main/vi/numbers.json'),
     require('cldr-data/main/vi/timeZoneNames.json'),
-    require('cldr-data/supplemental/weekdata.json'));
+    require('cldr-data/supplemental/weekData.json'));
 
     
 @NgModule({
--- App/src/app/views/pig-farm/system/system.module.ts
+++ App/src/app/views/pig-farm/system/system.module.ts
@@ -71,19 +71,19 @@ loadCldr(
   require('cldr-data/main/en/ca-gregorian.json'),
   require('cldr-data/main/en/numbers.json'),
   require('cldr-data/main/en/timeZoneNames.json'),
-  require('cldr-data/supplemental/weekdata.json')); // To load the culture based first day of week
+  require('cldr-data/supplemental/weekData.json')); // To load the culture based first day of week
   loadCldr(
     require('cldr-data/supplemental/numberingSystems.json'),
     require('cldr-data/main/zh/ca-gregorian.json'),
     require('cldr-data/main/zh/numbers.json'),
     require('cldr-data/main/zh/timeZoneNames.json'),
-    require('cldr-data/supplemental/weekdata.json')); 
+    require('cldr-data/supplemental/weekData.json')); 
     loadCldr(
       require('cldr-data/supplemental/numberingSystems.json'),
       require('cldr-data/main/vi/ca-gregorian.json'),
       require('cldr-data/main/vi/numbers.json'),
       require('cldr-data/main/vi/timeZoneNames.json'),
-      require('cldr-data/supplemental/weekdata.json'));
+      require('cldr-data/supplemental/weekData.json'));
     
 TreeGrid.Inject(Page, Toolbar, Filter);
 const REPORT_COMPONENT = [
